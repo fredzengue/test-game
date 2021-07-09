38ea337b6b6cbc0c10115539532bd0fab2d99c71
unknown <fredzengue@gmail.com>
2021-07-09 03:38:11 +0100

    compile asset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -139,9 +139,9 @@ a.disabled{
 }
 
     </style>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    <script src="http://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
