07894b85adcdde7f02d1cfb70d8fe7aee8e7e9c3
unknown <fredzengue@gmail.com>
2021-07-09 03:28:38 +0100

    compile asset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>TIC-TAC-TOE</title>
-    <link rel="stylesheet" href="{{ asset('css/') }}/app.css">
+    <link rel="stylesheet" href="{{ asset('css/master.css') }}">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
